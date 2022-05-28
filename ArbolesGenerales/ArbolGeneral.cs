c60215f65926b85ef23f8219d676c85efc66d549
HeryRobles <100168911+HeryRobles@users.noreply.github.com>
2022-05-28 13:09:55 -0700

    Creamos la Raiz del Arbol

---
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class ArbolGeneral
+    {
+        private readonly Nodo raiz;
+
+        //public Nodo Raiz => raiz; //(Hace lo mismo que el de abajo)
+        public Nodo Raiz { get { return raiz; } }
+
+        public ArbolGeneral(string dato)
+        {
+            raiz = new Nodo(dato);
+        }
+    }
+}
