c693b50cf8f7dca3c61112ee112a6dca06560830
Hery Robles <Hery Robles@HERYROBLES>
2022-06-04 20:21:39 -0500

    Repositorio Github

--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -8,8 +8,10 @@ Nodo nodoD = arbol.Insertar(nodoB, "D");
 Nodo nodoE = arbol.Insertar(nodoD, "E");
 Nodo nodoF = arbol.Insertar(nodoD, "F");
 Nodo nodoG = arbol.Insertar(nodoB, "G");
+Nodo nodoH = arbol.Insertar(nodoB, "H");
 
 
 string arbolCompleto = arbol.ObteberArbol();
 Console.WriteLine(arbolCompleto);
+
 
