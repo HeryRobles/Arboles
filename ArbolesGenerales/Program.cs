84911b71f32f88b51a698a5508c22afd57b3710b
Hery Robles <Hery Robles@HERYROBLES>
2022-06-12 17:49:00 -0500

    Agregamos Nuevo Proyecto de Arbol Binario y La Clase Nodo

--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -2,6 +2,23 @@
 
 ArbolGeneral arbol = new ArbolGeneral("A");
 
+//Console.WriteLine("* * * * A R B O L * G E N E R A L * * * *");
+//Console.WriteLine("Seleccionar el método que desea ejecutar");
+//int seleccion = int.Parse(string Console.ReadLine());
+//Console.WriteLine("1.  INSERTAR  NODO");
+//Console.WriteLine("2.  RECORRER  ÁRBOL");
+//Console.WriteLine("3.  OBTENER ÁRBOL");//Obteber Arbol
+//Console.WriteLine("4.  BUSCAR  NODO");//Buscar NODO
+
+//switch (seleccion)
+//{
+//    case 1:
+//        Insertar();
+
+//}
+
+
+
 Nodo nodoB = arbol.Insertar(arbol.Raiz, "B");
 Nodo nodoC = arbol.Insertar(arbol.Raiz, "C");
 Nodo nodoD = arbol.Insertar(nodoB, "D");
@@ -9,6 +26,7 @@ Nodo nodoE = arbol.Insertar(nodoD, "E");
 Nodo nodoF = arbol.Insertar(nodoD, "F");
 Nodo nodoG = arbol.Insertar(nodoB, "G");
 Nodo nodoH = arbol.Insertar(nodoB, "H");
+
 
 
 string arbolCompleto = arbol.ObteberArbol();
